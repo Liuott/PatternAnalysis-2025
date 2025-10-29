7301543a0e02465242e9da876473ace088619a35
Liuott <101920214+Liuott@users.noreply.github.com>
2025-10-30 04:51:23 +1000

    debug utils

--- recognition/hipmri-VAQVAE-48302304/utils.py
+++ recognition/hipmri-VAQVAE-48302304/utils.py
@@ -2,13 +2,17 @@ import torch
 from torchmetrics.functional.image.ssim import structural_similarity_index_measure as ssim_tm
 
 def img_to_01(x: torch.Tensor) -> torch.Tensor:
-    # x in [-1,1] -> [0,1]
     return (x + 1) / 2
 
 @torch.no_grad()
 def batch_ssim(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
     # x,y in [-1,1], shape (B,1,H,W)
-    x01, y01 = img_to_01(x), img_to_01(y)
+    x01 = img_to_01(x).clamp(0,1)
+    y01 = img_to_01(y).clamp(0,1)
+
+    eps = 1e-4
+    x01 = x01 + eps * (torch.rand_like(x01) - 0.5)
+    y01 = y01 + eps * (torch.rand_like(y01) - 0.5)
     return ssim_tm(x01, y01, data_range=1.0)
 
 def recon_loss_fn(kind='l1'):
