6ba1235f8875175aac78c1a0cc86c09369111396
Liuott <101920214+Liuott@users.noreply.github.com>
2025-10-30 01:25:51 +1000

    update

--- recognition/hipmri-VAQVAE-48302304/dataset.py
+++ recognition/hipmri-VAQVAE-48302304/dataset.py
@@ -18,7 +18,18 @@ class HipMRI2DImageOnly(Dataset):
         if arr.ndim==3: 
             arr = arr[:,:,0]
         return arr.astype(np.float32)
-
+    
+    def split_indices(files, val_ratio=0.15, test_ratio=0.15, seed=2025):
+        import random
+        rng = random.Random(seed); 
+        idx = list(range(len(files))); 
+        rng.shuffle(idx)
+        n = len(idx); 
+        n_te = int(n*test_ratio); 
+        n_va = int(n*val_ratio)
+        te, va, tr = idx[:n_te], idx[n_te:n_te+n_va], idx[n_te+n_va:]
+        return tr, va, te
+    
     def __getitem__(self, i):
         ip = self.files[i]
         img = self._read_nii2d(ip)
@@ -33,16 +44,11 @@ class HipMRI2DImageOnly(Dataset):
         img = torch.from_numpy(img[None,...]).float()
         return img, os.path.basename(ip)
     
-    def _split(files, val_ratio=0.15, test_ratio=0.15, seed=2025):
-        import random
-        rng = random.Random(seed); idx = list(range(len(files))); rng.shuffle(idx)
-        n = len(idx); n_te = int(n*test_ratio); n_va = int(n*val_ratio)
-        te, va, tr = idx[:n_te], idx[n_te:n_te+n_va], idx[n_te+n_va:]
-        return tr, va, te
+
     
     def build_loaders(root, img_dir, img_size, batch_size, val_ratio, test_ratio, seed, num_workers):
         files = sorted(glob.glob(os.path.join(root, img_dir, '*.nii*')))
-        tr, va, te = _split(files, val_ratio, test_ratio, seed)
+        tr, va, te = split_indices(files, val_ratio, test_ratio, seed)
         get = lambda ids, aug: HipMRI2DImageOnly(root, img_dir, img_size, files=[files[i] for i in ids], aug=aug)
         ds_tr, ds_va, ds_te = get(tr, True), get(va, False), get(te, False)
         L = lambda ds, s: DataLoader(ds, batch_size=batch_size, shuffle=(s=='tr'), num_workers=num_workers, pin_memory=True)
